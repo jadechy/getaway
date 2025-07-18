80323f52f5fc0d79e604f404b9cfaeb92d593f53
Jade Chi-yen <chiyenjade@gmail.com>
2025-07-18 12:24:30 +0200

    wip: welcome and home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const router = useRouter();
+</script>
 
 <template>
-  <p>welcome index</p>
+  <h2>Bienvenue sur Getaway</h2>
+  <p>Avec Getaway créé des sorties inoubliables sur mesure</p>
+  <button @click="router.push({ name: 'auth-register' })">Créer un compte gratuitement</button>
+  <button @click="router.push({ name: 'auth-login' })">Se connecter</button>
 </template>
 
 <style></style>
